10569cccebf2b43c5233fe557e57398ea4d8cd0a
bill <william234@aol.com>
2019-09-09 22:05:11 -0400

    Add a unit test

--- HackerNews.Tests/TestTimeConversion.cs
+++ HackerNews.Tests/TestTimeConversion.cs
@@ -1,14 +1,24 @@
 using System;
+using System.Diagnostics;
+using HackerNews.Web.Logic.Utilities;
+using Moq;
 using Xunit;
 
 namespace HackerNews.Tests
 {
-    public class UnitTest1
+    public class TestTimeConversion
     {
-        [Fact]
-        public void Test1()
+
+        [Theory]
+        [InlineData(1568079800, "01:43:20")]
+        public void TestTimeConversionFromUnixTimeStamp(int timeStamp, string expected)
         {
 
+            var storyMapper = new StoryMapper();
+            var outDateTime = storyMapper.GetDotNetTimeFromUnixTime(timeStamp);
+            Assert.Equal(outDateTime.ToLongTimeString(), expected);
+
         }
+
     }
 }
